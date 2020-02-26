66d5a041acf48ea1b0cdcc2723ae260ab97381b6
m1015 <Luiz10284355>
2020-02-26 15:33:32 -0300

    Movendo arquivo apenas se existir anexo para Faturas_L

--- Montreal_Download_Anexos_E_mail/Download_Attachments.py
+++ Montreal_Download_Anexos_E_mail/Download_Attachments.py
@@ -47,15 +47,15 @@ for uid_emails in emails_inbox:
     if date_e_mail == datetime.datetime.now().date():
         for part in raw.walk():
             attachment_name = part.get_filename()
-            if attachment_name and os.path.splitext(attachment_name)[1] in '.pdf':
+            if attachment_name and os.path.splitext(attachment_name)[1].lower() in '.pdf':
                 attachment_name = mount_attachment_name(attachment_name, subject)
                 dire = mount_dir(date_e_mail, attachment_name)
-                print(dire)
                 with open(dire, 'wb') as file:
                     file.write(part.get_payload(decode=True))
-        print(f'Movendo e-mail {subject} para {move_folder}\n')
-        result = mail.uid('COPY', uid_emails, move_folder)
-
-        if result[0] == 'OK':
-            mov, data = mail.uid('STORE', uid_emails, '+FLAGS', '(\\Deleted)')
-            mail.expunge()
+                    print('Download feito com sucesso em,', dire)
+                    print(f'Movendo e-mail "{subject}" para {move_folder}')
+                    result = mail.uid('COPY', uid_emails, move_folder)
+                    if result[0] == 'OK':
+                        mov, data = mail.uid('STORE', uid_emails, '+FLAGS', '(\\Deleted)')
+                        mail.expunge()
+        print()
