4c4a2d0166547325ac78db9d6441df2d84a10627
m1015 <Luiz10284355>
2020-03-10 17:52:05 -0300

    Adicionando trusted_connection

--- Banco/Conect.py
+++ Banco/Conect.py
@@ -2,25 +2,28 @@ import pyodbc, pandas as pd
 
 
 class Conect(object):
+
     def __init__(self, driver='{SQL Server Native Client 11.0}',
                  server='cltsql01',
                  database='montreal',
-                 usuario='web_usr',
-                 pasword='Ch4rm8s8'):
-
+                 usuario='0',
+                 pasword='0', trusted_connection=False):
         self.driver = driver
         self.server = server
         self.database = database
         self.usuario = usuario
         self.pasword = pasword
-        self.conexao = pyodbc.connect(f'DRIVER={self.driver};'
-                                      f'SERVER={self.server};'
-                                      f'DATABASE={self.database};'
-                                      f'UID={self.usuario};'
-                                      f'PWD={self.pasword}', autocommit=True)
+        connect_inf = f'''DRIVER={self.driver};
+SERVER={self.server};
+DATABASE={self.database};'''
+        if trusted_connection:
+            connect_inf += '\nTrusted_Connection=yes;'
+        else:
+            connect_inf += f'\nUID={self.usuario};\nPWD={self.pasword};'
+        self.conexao = pyodbc.connect(connect_inf, autocommit=True)
         self.cursor = self.conexao.cursor()
 
-    def _exec_query(self, query):
+    def _exec_query(self, query) -> pd:
         print(query)
         return pd.read_sql(query, self.conexao)
 
