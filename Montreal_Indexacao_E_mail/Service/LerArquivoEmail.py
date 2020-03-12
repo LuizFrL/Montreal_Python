b7c3a118fcee8445f5e3d91758f4a5a94a44d93b
m1015 <Luiz10284355>
2020-03-12 14:47:56 -0300

    Criacao de id's

--- Montreal_Indexacao_E_mail/Service/LerArquivoEmail.py
+++ Montreal_Indexacao_E_mail/Service/LerArquivoEmail.py
@@ -1,12 +1,10 @@
-import email, glob, os
+import email, os
 import hashlib
 
 
 class LerArquivoEmail(object):
 
     def __init__(self, arquivo):
-        self.id_from = self.create_id_from()
-        self.id_mensagem = self.create_id_mensagem()
         self.dir_arquivo = arquivo
         self.msg = self.get_msg_email(arquivo)
 
@@ -14,6 +12,18 @@ class LerArquivoEmail(object):
     def get_msg_email(dir_arquivo):
         with open(dir_arquivo, 'r', encoding='utf-8') as f_email:
             return email.message_from_file(f_email)
+
+    @staticmethod
+    def gerar_hash_md5(informacoes):
+        return hashlib.md5(str(informacoes).encode()).hexdigest()
+
+    @staticmethod
+    def __get_size_attachment(name, payload):
+        with open(name, 'w') as f:
+            f.write(payload)
+            size = os.stat(name).st_size
+        os.unlink(name)
+        return size
 
     def get_email_from(self):
         return self.msg['From']
@@ -50,7 +60,7 @@ class LerArquivoEmail(object):
         for pa in self.msg.walk():
             if pa.get_filename():
                 attachment_content_type = {
-                    'id': self.create_id_anexo(),
+                    'id': self.create_id_anexo(pa.get_payload()),
                     'Name': pa.get_filename(),
                     'Content_Type': pa.get_content_type(),
                     'Size': self.__get_size_attachment(pa.get_filename(), pa.get_payload()),
@@ -59,24 +69,32 @@ class LerArquivoEmail(object):
                 inf_attachments.append(attachment_content_type)
         return inf_attachments
 
-    def __get_size_attachment(self, name, payload):
-        with open(name, 'w') as f:
-            f.write(payload)
-            size = os.stat(name).st_size
-        os.unlink(name)
-        return size
-
     def create_id_email(self):
-        pass
+        """
+        ID gerado com a juncao do hash md5 do texto escrito no email (campo com as informacoes retornadas da funcao
+        get_email_text()) e com o hash md5 do seu payload (campo com as informacoes contidas na funcao get_email_html())
+        """
+        return self.gerar_hash_md5(self.get_email_text()) + self.gerar_hash_md5(self.get_email_html())
 
-    def create_id_anexo(self) -> str:
-        pass
+    def create_id_anexo(self, payload) -> str:
+        """
+        ID gerado com a junção do hash md5 do id_email (retornado da funcao create_id_email()) com o hash md5
+        das informacoes dentro do anexo (payload do anexo).
+        """
+        return self.gerar_hash_md5(self.create_id_email()) + self.gerar_hash_md5(payload)
 
     def create_id_mensagem(self) -> str:
-        pass
+        """
+        ID gerado com a juncao do Id_email (funcao create_id_email()) com o hash md5 com as informacoes da
+        funcao get_email_message_id()
+        """
+        return self.create_id_email() + self.gerar_hash_md5(self.get_email_message_id())
 
     def create_id_from(self) -> str:
-        pass
+        """
+        ID gerado com a informacao gerada pelo hash md5 da funcao get_email_from()
+        """
+        return self.gerar_hash_md5(self.get_email_from())
 
 
 if __name__ == '__main__':
